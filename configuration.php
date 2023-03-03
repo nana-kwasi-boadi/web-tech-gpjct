406585e3315687d1f118d6e791fa3e5240cea183
Nana Kwasi Boadi <Enkaybee@NanaAir-4.local>
2023-03-03 12:28:52 +0000

    made some changes

--- configuration.php
+++ configuration.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "carmanufacturingcompany";
+$dbname = "carManufacturingCompany";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
